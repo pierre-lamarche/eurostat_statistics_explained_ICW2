7bb446489592914ed0cb25904787d659dca61f31
pierre-lamarche <lamarche.p@gmail.com>
2017-07-04 10:06:21 +0200

    Figure 3

--- generate figures and graphs.R
+++ generate figures and graphs.R
@@ -48,3 +48,29 @@ barplot(t(figure2[,2:3]), beside = TRUE, col = c(col1, col2), main = NA,
         border = NA, legend.text = c("Median saving rate", "Aggregate saving rate"),
         names.arg = figure2$geo, cex.names = 0.5, ylim = c(-20,50), cex.axis = 0.7,
         args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
+
+
+#################################################################################################################################################
+### FIGURE 3
+#################################################################################################################################################
+
+indicators_sr <- get_eurostat("icw_sr_08", time_format = "num")
+sr_na <- get_eurostat("nasa_10_ki", time_format = "num")
+sr_agg <- filter(indicators_sr,
+                 age == "TOTAL")
+sr_na_2010 <- filter(sr_na,
+                     time == 2010 & sector == "S14_S15" & na_item == "SRG_S14_S15")
+
+sr_na_2010 <- mutate(sr_na_2010,
+                     sr_na = values)
+sr_agg <- mutate(sr_agg,
+                 sr_agg = 100 - values)
+sr <- merge(sr_agg, sr_na_2010, by = "geo")
+
+plot(sr$sr_agg, sr$sr_na, type = "p", pch = 18, col = col1, bty = "n", xaxt="n",yaxt="n", 
+     xlim = c(0,50), ylim = c(-5,20), xlab = NA, ylab = NA)
+grid(nx = NA, ny = NULL)
+axis(1,pos=0)
+axis(2,pos=0, tick = FALSE, las = 1)
+text(sr$sr_agg, sr$sr_na, labels = sr$geo, cex = 0.7, pos = 3)
+
