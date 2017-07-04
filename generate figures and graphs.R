4ac3d094c003280eead755cf5f8ab1809866599b
pierre-lamarche <lamarche.p@gmail.com>
2017-07-04 10:51:41 +0200

    Add considerations on imputed rents

--- generate figures and graphs.R
+++ generate figures and graphs.R
@@ -74,3 +74,78 @@ axis(1,pos=0)
 axis(2,pos=0, tick = FALSE, las = 1)
 text(sr$sr_agg, sr$sr_na, labels = sr$geo, cex = 0.7, pos = 3)
 
+
+### alternative: account for imputed rents
+
+accounts_NA <- get_eurostat("nasa_10_nf_tr", time_format = "num")
+
+# imputed rents (B2G)
+b2g_s1415 <- filter(accounts_NA,
+                    unit == "CP_MEUR" & na_item == "B2G" & sector == "S14_S15")
+b2g_s1415_paid <- filter(b2g_s1415,
+                         direct == "PAID")
+b2g_s1415_received <- filter(b2g_s1415,
+                             direct == "RECV")
+b2g_s1415_paid <- mutate(b2g_s1415_paid,
+                         b2g_paid = as.numeric(values))
+b2g_s1415_received <- mutate(b2g_s1415_received,
+                             b2g_received = as.numeric(values))
+b2g_s1415_paid <- b2g_s1415_paid[, c("geo","time","sector","unit","b2g_paid")]
+b2g_s1415_received <- b2g_s1415_received[, c("geo","time","sector","b2g_received")]
+b2n_s1514 <- merge(b2g_s1415_paid, b2g_s1415_received)
+
+# expenditures (P31)
+p31_s1415 <- filter(accounts_NA,
+                    unit == "CP_MEUR" & na_item == "P31" & sector == "S14_S15")
+p31_s1415 <- mutate(p31_s1415,
+                    p31 = as.numeric(values))
+p31_s1415 <- p31_s1415[, c("geo","time","sector","p31")]
+
+# gross disposable income (B6G)
+b6g_s1415 <- filter(accounts_NA,
+                    unit == "CP_MEUR" & na_item == "B6G" & sector == "S14_S15" & direct == "RECV")
+b6g_s1415 <- mutate(b6g_s1415,
+                    b6g = as.numeric(values))
+b6g_s1415 <- b6g_s1415[, c("geo","time","sector","b6g")]
+
+# adjustement for the change in net equity of pensions funds
+d8_s1415 <- filter(accounts_NA,
+                   unit == "CP_MEUR" & na_item == "D8" & sector == "S14_S15")
+d8_s1415_paid <- filter(d8_s1415,
+                        direct == "PAID")
+d8_s1415_received <- filter(d8_s1415,
+                            direct == "RECV")
+d8_s1415_paid <- mutate(d8_s1415_paid,
+                        d8_paid = as.numeric(values))
+d8_s1415_received <- mutate(d8_s1415_received,
+                            d8_received = as.numeric(values))
+d8_s1415_paid <- d8_s1415_paid[, c("geo","time","sector","unit","d8_paid")]
+d8_s1415_received <- d8_s1415_received[, c("geo","time","sector","unit","d8_received")]
+# merge, assuming missing as 0
+d8n_s1415 <- merge(d8_s1415_paid, d8_s1415_received, all = TRUE)
+d8n_s1415 <- mutate(d8n_s1415,
+                    d8_received = ifelse(is.na(d8_received), 0, d8_received),
+                    d8_paid = ifelse(is.na(d8_paid), 0, d8_paid),
+                    d8n = d8_received - d8_paid)
+d8n_s1415 <- d8n_s1415[, c("geo","time","sector","d8n")]
+
+b8g_s1415 <- merge(p31_s1415, b6g_s1415)
+b8g_s1415 <- merge(b8g_s1415, d8n_s1415)
+b8g_s1415 <- merge(b8g_s1415, b2g_s1415_received)
+
+b8g_s1415 <- mutate(b8g_s1415,
+                    b8g = b6g + d8n - p31,
+                    saving_rate_na = round(100*b8g/(b6g + d8n), digits = 1),
+                    saving_rate_wo_imprent = round(100*b8g/(b6g + d8n - b2g_received), digits = 1)
+)
+
+sr <- merge(sr_agg, b8g_s1415,
+            by = c("geo","time"))
+
+plot(sr$sr_agg, sr$saving_rate_wo_imprent, type = "p", pch = 18, col = col1, bty = "n", xaxt="n",yaxt="n", 
+     xlim = c(0,50), ylim = c(-5,20), xlab = NA, ylab = NA)
+grid(nx = NA, ny = NULL)
+axis(1,pos=0)
+axis(2,pos=0, tick = FALSE, las = 1)
+text(sr$sr_agg, sr$saving_rate_wo_imprent, labels = sr$geo, cex = 0.7, pos = 3)
+
